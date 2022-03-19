6c97f964add13268dd788c2c65d50c98e1c54c34
sadia71 <sadia.afrin1.cse@ulab.edu.bd>
2022-03-19 15:19:08 +0600

    login Page design is done

--- lib/routes/LoginPage.dart
+++ lib/routes/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -11,9 +12,94 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        alignment: Alignment.center,
-        child: const Text('Log In'),
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              Container(
+                alignment: Alignment.center,
+                child: Center(
+                    child:Container(
+                        height: 180,
+                        width: 180,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(0.3),
+                                  spreadRadius: 1,
+                                  blurRadius: 5,
+                                  offset: const Offset(0, 3)
+                              )
+                            ]
+                        ),
+                        child: Lottie.asset('assets/auction_lottie_haggle-bd.json')
+                    )
+                ) ,
+              ),
+              const SizedBox(height: 15.0),
+              Center( child: Stack(
+                children: <Widget>[
+                  // Stroked text as border.
+                  Text(
+                    'Haggle BD',
+                    style: TextStyle(
+                      fontSize: 40,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 5
+                        ..color = Colors.blue[500]!,
+                    ),
+                  ),
+                  // Solid text as fill.
+                  Text(
+                    'Haggle BD',
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                ],
+              )),
+              const SizedBox(height: 5.0),
+              const Center(child: Text('BUY AND SELL PRODUCT', style: TextStyle(fontSize: 18, ),textAlign: TextAlign.right,)),
+
+            ],
+          )
       ),
+
+      floatingActionButton: FloatingActionButton.extended(onPressed: () async {
+        // try{
+        //   await _signInWithGoogle();
+        //
+        // } catch (e){
+        //   print(e);
+        // }
+      },
+          label: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('SIGN IN WITH', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              Container(
+                height: 40,
+                width: 40,
+                margin: const EdgeInsets.only(left: 10.0),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/google_icon.png'),
+                    )
+                ),
+              )
+
+            ],
+          )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+
     );
   }
 }
