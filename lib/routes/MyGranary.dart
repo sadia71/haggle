3376c3e313c6a7f620189684103b206ebae61f4f
sadia71 <sadia.afrin1.cse@ulab.edu.bd>
2022-04-02 13:28:36 +0600

    all things implemented

--- lib/routes/MyGranary.dart
+++ lib/routes/MyGranary.dart
@@ -1,14 +1,132 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:haggle/utilities/GesturedCard.dart';
+
+
+var _selectOptionBid = false;
+var _selectOptionItem = true;
+
 class MyGranary extends StatefulWidget {
   const MyGranary({Key? key}) : super(key: key);
+
 
   @override
   _MyGranaryState createState() => _MyGranaryState();
 }
 
 class _MyGranaryState extends State<MyGranary> {
+
   @override
   Widget build(BuildContext context) {
-    return const Text('MyGranary');
+
+    User? currentUser = FirebaseAuth.instance.currentUser;
+
+    var raisedButtonStyle = ElevatedButton.styleFrom(
+      onPrimary: _selectOptionBid ? Colors.white : Colors.blue[500] ,
+      primary: _selectOptionBid ? Colors.blue[500] : Colors.white,
+      minimumSize: Size(88, 36),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+      ),
+    );
+
+    var raisedButtonStyle2 = ElevatedButton.styleFrom(
+      onPrimary: _selectOptionItem ? Colors.white : Colors.blue[500] ,
+      primary: _selectOptionItem ? Colors.blue[500] : Colors.white,
+      minimumSize: Size(88, 36),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+      ),
+    );
+
+    return  Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          title: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    style: raisedButtonStyle2,
+                    onPressed: () {
+                      setState(() {
+                        _selectOptionBid = false;
+                        _selectOptionItem = true;
+                      });
+
+                    }
+                    ,
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                           Icon(
+                            Icons.sell,
+                            color: _selectOptionItem ? Colors.white : Colors.blue[500],
+                            size: 20.0,
+                          ),
+                          Text('My Items', style: TextStyle(fontSize: 18),)
+                        ],
+
+                      ),
+                    )
+                ),
+                ElevatedButton(
+                    style: raisedButtonStyle,
+                    onPressed: () {
+                      setState(() {
+                        _selectOptionItem = false;
+                        _selectOptionBid = true;
+                      });
+
+
+                    },
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                           Icon(
+                            Icons.attach_money,
+                            color: _selectOptionBid ? Colors.white : Colors.blue[500],
+                            size: 20.0,
+                          ),
+                          Text('My Bids', style: TextStyle(fontSize: 18),)
+                        ],
+
+                      ),
+                    )
+                )
+
+              ],
+            ),
+          ),
+        ),
+        body: _selectOptionItem ? StreamBuilder <QuerySnapshot> (
+            stream: FirebaseFirestore.instance
+                .collection('items').where('userId', isEqualTo: currentUser!.uid).snapshots(includeMetadataChanges: true),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+              return snapshot.hasData ?
+              GesturedCard(item: snapshot.data!.docs) :
+              Center( child: CircularProgressIndicator(color: Colors.blue[500],));
+            }
+        ) :
+        StreamBuilder <QuerySnapshot> (
+            stream: FirebaseFirestore.instance
+                .collection('items').where('bidUsers', arrayContains: currentUser!.uid).snapshots(includeMetadataChanges: true),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+              return snapshot.hasData ?
+            GesturedCard(item: snapshot.data!.docs) :
+              Center( child: CircularProgressIndicator(color: Colors.blue[500],));
+            },
+
+        ));
   }
 }
--- lib/utilities/GesturedCard.dart
+++ lib/utilities/GesturedCard.dart
@@ -1,122 +1,121 @@
-//
-//
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:haggle/imports/firebase/UserManagement.dart';
-// import 'package:haggle/navigation/BidsItemDetails.dart';
-// import 'package:haggle/imports/utilities/Carousel.dart';
-// import 'AuctionTime.dart';
-// class GesturedCard extends StatefulWidget {
-//   final item;
-//   GesturedCard(this.item);
-//
-//   @override
-//   _GesturedCardState createState() => _GesturedCardState();
-// }
-//
-// class _GesturedCardState extends State<GesturedCard> {
-//   @override
-//   Widget build(BuildContext context) {
-//     var item = widget.item;
-//     User? currentUser = FirebaseAuth.instance.currentUser;
-//
-//
-//     return new ListView.builder(
-//         itemCount: item.length,
-//         key: UniqueKey(),
-//         itemBuilder: (context, index) {
-//           var itemData = item[index].data();
-//           return GestureDetector(
-//             onTap: () {
-//               Navigator.of(context).push(MaterialPageRoute(builder: (context) =>
-//                   BidsItemDetails(itemDetails: item[index])));
-//             },
-//             child : Card(
-//               clipBehavior: Clip.antiAlias,
-//               child: Column(
-//                 children: <Widget>[
-//                   Container(
-//                       padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-//                       child: Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                         children: [
-//                           UserManagement().getPostedUser(itemData['userId'], 'MODERATE_NAME', 10.0, 14.0, false, false, true),
-//                           Text(AuctionTime().getPostedDay(itemData['bidAt']))
-//                         ],
-//                       )
-//
-//                   ),
-//                   Carousel().imageCarousel(itemData['itemImages'], 180.0),
-//                   Container(
-//                     child: AuctionTime().getCountDown(itemData['lastBidTime'], itemData['itemId'], itemData['isCompleted']),
-//                   ),
-//
-//                   Container(
-//                     padding: EdgeInsets.only(top: 0, bottom: 10),
-//                     child: Column(
-//                       mainAxisAlignment: MainAxisAlignment.start,
-//                       children: [
-//                         Container(
-//                           padding: EdgeInsets.symmetric(horizontal: 10),
-//                           child:Text(itemData['itemName'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//                           alignment: Alignment.topLeft,
-//                         ),
-//
-//                         Container(
-//                             padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-//                             child: Row(
-//                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                               children: [
-//                                 Text(
-//                                   'Min Bid: \$${itemData["minBidPrice"]}',
-//                                   style: TextStyle(color: Colors.black.withOpacity(0.6), fontWeight: FontWeight.bold),
-//                                 ),
-//
-//                                 Text(
-//                                   'last time : '+ AuctionTime().getTime(itemData['lastBidTime']),
-//                                   style: TextStyle(color: Colors.black.withOpacity(0.6)),
-//                                 ),
-//                               ],
-//                             )
-//
-//                         ),
-//
-//                       ],
-//                     ),
-//                   ),
-//                   Container(
-//                     child: Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         Container(
-//                           padding: EdgeInsets.only(left: 10.0, bottom: 10.0),
-//                           child: Row(
-//                             children: [
-//                               Text(itemData.containsKey('bidUsers') && itemData['bidUsers'].length > 0 ? itemData['bidUsers'].length.toString() : '0'),
-//                               Icon(
-//                                 Icons.people,
-//                                 size: 15,
-//                                 color: Colors.black.withOpacity(0.5),
-//                               )
-//                             ],
-//                           )
-//                         ),
-//                         Container(
-//                           padding: EdgeInsets.only(right: 10.0, bottom: 10.0),
-//                           child: Icon(
-//                             Icons.monetization_on,
-//                             color: itemData.containsKey('bidUsers') && itemData['bidUsers'].length > 0 && itemData['bidUsers'].contains(currentUser!.uid)
-//                                 ? Colors.orange : Colors.grey.withOpacity(0.5),
-//                           )
-//                         ),
-//                       ],
-//                     ),
-//                   )
-//                 ],
-//               ),
-//             ),
-//           );
-//         });
-//
-//   }
-// }
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:haggle/firebase/UserManagement.dart';
+import 'package:haggle/routes/BidsItemDetails.dart';
+
+import 'package:haggle/utilities/Carousel.dart';
+import 'AuctionTime.dart';
+class GesturedCard extends StatefulWidget {
+  final item;
+  const GesturedCard({Key? key, @required this.item}) : super(key: key);
+
+  @override
+  _GesturedCardState createState() => _GesturedCardState();
+}
+
+class _GesturedCardState extends State<GesturedCard> {
+  @override
+  Widget build(BuildContext context) {
+    var item = widget.item;
+    User? currentUser = FirebaseAuth.instance.currentUser;
+
+
+    return  ListView.builder(
+        itemCount: item.length,
+        key: UniqueKey(),
+        itemBuilder: (context, index) {
+          var itemData = item[index].data();
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) =>
+                  BidsItemDetails(itemDetails: item[index])));
+            },
+            child : Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          UserManagement().getPostedUser(itemData['userId'], 'MODERATE_NAME', 10.0, 14.0, false, false, true),
+                          Text(AuctionTime().getPostedDay(itemData['bidAt']))
+                        ],
+                      )
+
+                  ),
+                  Carousel().imageCarousel(itemData['itemImages'], 180.0),
+                  Container(
+                    child: AuctionTime().getCountDown(itemData['lastBidTime'], itemData['itemId'], itemData['isCompleted']),
+                  ),
+
+                  Container(
+                    padding: const EdgeInsets.only(top: 0, bottom: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child:Text(itemData['itemName'], style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                          alignment: Alignment.topLeft,
+                        ),
+
+                        Container(
+                            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Min Bid: ৳${itemData["minBidPrice"]}',
+                                  style: TextStyle(color: Colors.black.withOpacity(0.6), fontWeight: FontWeight.bold),
+                                ),
+
+                                Text(
+                                  'last time : '+ AuctionTime().getTime(itemData['lastBidTime']),
+                                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ],
+                            )
+
+                        ),
+
+                      ],
+                    ),
+                  ),
+                Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(left: 10.0, bottom: 10.0),
+                          child: Row(
+                            children: [
+                              Text(itemData.containsKey('bidUsers') && itemData['bidUsers'].length > 0 ? itemData['bidUsers'].length.toString() : '0'),
+                              Icon(
+                                Icons.people,
+                                size: 15,
+                                color: Colors.black.withOpacity(0.5),
+                              )
+                            ],
+                          )
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
+                          child: Icon(
+                            Icons.monetization_on,
+                            color: itemData.containsKey('bidUsers') && itemData['bidUsers'].length > 0 && itemData['bidUsers'].contains(currentUser!.uid)
+                                ? Colors.orange : Colors.grey.withOpacity(0.5),
+                          )
+                        ),
+                      ],
+                    ),
+                ],
+              ),
+            ),
+          );
+        });
+
+  }
+}
