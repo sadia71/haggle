7dfdaa232de896ee0f143b25e416db9c6c0e53e2
Sadia Afrin <sadia.afrin1.cse@ulab.edu.bd>
2022-04-13 19:11:55 +0600

    now app run in windows

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/sria/FLUTTER_SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/sria/StudioProjects/haggle"
+export "FLUTTER_ROOT=C:\Users\sadia\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sadia\gitHub\haggle"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
